7b25676215c247d8a778ed1585f08d569475039d
javier navas <j.navasdam@gmail.com>
2025-10-15 20:00:51 +0200

    Cambios en la variable global PORT

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const PORT = 4000
+const dotenv=require('dotenv').config();
+const PORT = 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.listen(PORT, () => {
-  console.log(`App escuchando en puerto ${PORT}`)
+  console.log(`App escuchando en puerto ${process.env.PORT}`)
 })
 //npm run dev
